50175718088774968c3b3b6454a4c37d01df1da1
PaulinedeValance <pauline.de.valance@gmail.com>
2023-05-09 18:08:06 +0200

    changement nom bis

--- src/controllers/gamesController.ts
+++ src/controllers/gamesController.ts
@@ -1,9 +1,9 @@
-import jeu from "../models/gamesModel";
+import game from "../models/gamesModel";
 import { Request, Response } from "express";
 
-export const ajouterJeu = async (req: Request, res: Response) => {
+export const addGame = async (req: Request, res: Response) => {
   //console.log(req.body);
-  const nouveauJeu = new jeu({
+  const newGame = new game({
     nom: req.body.nom,
     editeur: req.body.editeur,
     nbJoueurs: req.body.nbJoueurs,
@@ -13,16 +13,16 @@ export const ajouterJeu = async (req: Request, res: Response) => {
   });
 
   try {
-    const jeuExistant = await jeu.findOne({ nom: nouveauJeu.nom });
+    const existingGame = await game.findOne({ nom: newGame.nom });
 
-    if (jeuExistant) {
+    if (existingGame) {
       // Le jeu existe déjà dans la base de données
       console.log("Le jeu existe déjà !");
       req.flash("error", "Le jeu existe déjà !"); // message flashbag
       res.redirect("/game/add"); // Rediriger vers le formulaire d'ajout de jeu
     } else {
       // Le jeu n'existe pas encore dans la base de données
-      await nouveauJeu.save();
+      await newGame.save();
       console.log("Le jeu a été ajouté !");
       req.flash("success", "Le jeu a été ajouté avec succès !"); // message flashbag
       res.redirect("/jeux"); // Rediriger l'utilisateur vers la liste des jeux
@@ -32,11 +32,11 @@ export const ajouterJeu = async (req: Request, res: Response) => {
   }
 };
 
-export const supprimerJeu = async (req: Request, res: Response) => {
-  const jeuId = req.params.jeuId;
+export const deleteGame = async (req: Request, res: Response) => {
+  const idGame = req.params.idGame;
 
   try {
-    await jeu.findByIdAndRemove(jeuId);
+    await game.findByIdAndRemove(idGame);
     console.log("Le jeu a été supprimé !");
     req.flash("success", "Le jeu a été supprimé avec succès !");
     res.redirect("/jeux");
@@ -45,11 +45,4 @@ export const supprimerJeu = async (req: Request, res: Response) => {
   }
 };
 
-// export const modifierJeu = async (req: Request, res: Response) => {
-//   try {
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-export default { ajouterJeu, supprimerJeu };
+export default { addGame, deleteGame };
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import passport from "passport";
-import jeuxController from "./controllers/jeuxController";
-import { supprimerJeu } from "./controllers/jeuxController";
-import displayJeuxController from "./controllers/displayJeuxController";
+import gamesController, { deleteGame } from "./controllers/gamesController";
+//import { deleteGame } from "./controllers/gamesController";
+import displayGamesController from "./controllers/displayGamesController";
 
 const router = express.Router();
 
@@ -23,7 +23,7 @@ router.get("/game/modify", (req: any, res: any) => {
 });
 
 // Route pour SUPPRIMER un jeu
-router.get("/game/delete/:jeuId", supprimerJeu);
+router.get("/game/delete/:idGame", deleteGame);
 
 // Route qui gère l'authentification et différentes redirections
 router.post(
@@ -36,12 +36,12 @@ router.post(
 );
 
 // Route pour ajouter un jeu dans la BDD
-router.post("/ajouter-jeu", jeuxController.ajouterJeu);
+router.post("/ajouter-jeu", gamesController.addGame);
 
 // Route pour modifier un jeu dans la BDD
 //router.post("/modifier-jeu", jeuxController.modifierJeu);
 
 // Route qui affiche la liste des jeux rentrées dans la BDD
-router.get("/jeux", displayJeuxController.afficherListeJeux);
+router.get("/jeux", displayGamesController.afficherListeJeux);
 
 export default router;
