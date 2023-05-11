7b3ecd9215ae9299db0fa96e3ce4236e6b06f784
PaulinedeValance <pauline.de.valance@gmail.com>
2023-05-11 16:25:08 +0200

    création d'une str aleatoire pour mes noms de jeux

--- uploadFile.ts
+++ uploadFile.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as AWS from "aws-sdk";
+import * as path from "path";
 
 // Récupération des clés d'accès de l'utilisateur
 const IAM_USER_KEY = process.env.AWS_ACCESS_KEY_ID;
@@ -16,13 +17,25 @@ export function uploadToS3(
   fileName: string,
   originalFileName: string
 ): Promise<any> {
+  // Création d'une string aléatoire avec des chiffres et lettres de 8 caractères
+  const randomString = Math.random().toString(36).slice(2, 10);
   // Création d'un flux de lecture à partir du fichier local
   const readStream = fs.createReadStream(fileName);
+
+  // J'obtient l'extension du fichier
+  const fileExtension = path.extname(originalFileName);
+  // J'obtient le nom du fichier sans l'extension
+  const fileNameWithoutExtension = path.basename(
+    originalFileName,
+    fileExtension
+  );
+  // concaténation pour obtenir le nom du fichier puis la string aléatoire et enfin l'extension de mon fichier
+  const newFileName = `${fileNameWithoutExtension}-${randomString}${fileExtension}`;
 
   // Définition des paramètres pour l'opération d'upload
   const params = {
     Bucket: "ludostockage",
-    Key: "imagesJeu/" + originalFileName,
+    Key: `imagesJeu/${newFileName}`,
     Body: readStream,
   };
 
