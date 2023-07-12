aeceead4488c20e310f1af2badfa7dbbe475bdc7
PaulinedeValance <pauline.de.valance@gmail.com>
2023-07-12 17:03:03 +0200

    modifier un jeu, working

--- src/api/apiRoutes.ts
+++ src/api/apiRoutes.ts
@@ -1,43 +1,46 @@
-import { addGame, deleteGame, editGame, uploadImages } from "./gamesController";
-import Game from "../models/gamesModel";
-import express, { Request, Response } from "express";
-import multer from "multer";
-import passport from "passport";
-import usersController from "../controllers/usersController";
+import { addGame, deleteGame, editGame2, uploadImages } from './gamesController'
+import Game from '../models/gamesModel'
+import express, { Request, Response } from 'express'
+import multer from 'multer'
+import passport from 'passport'
+import usersController from '../controllers/usersController'
 
-const router = express.Router();
-const upload = multer({ dest: "uploads/" });
+const router = express.Router()
+const upload = multer({ dest: 'uploads/' })
 
 ////////////
 /// GAME ///
 ////////////
 
 // Route pour récupérer tous les jeux
-router.get("/games", async (req, res) => {
-  const games = await Game.find();
-  return res.json(games);
-});
-
-//router.post("/games", addGame);
+router.get('/games', async (req, res) => {
+  const games = await Game.find()
+  return res.json(games)
+})
 
 // Route pour ajouter un jeu dans la BDD
-router.post("/games", async (req: Request, res: Response) => {
-  const isNewGame = await addGame(req.body);
+router.post('/games', async (req: Request, res: Response) => {
+  const isNewGame = await addGame(req.body)
   if (isNewGame) {
-    res.redirect("/game/add");
+    res.redirect('/game/add')
   } else {
-    res.redirect("/games");
+    res.redirect('/games')
   }
-});
+})
 
 // Route pour modifier un jeu dans la BDD
-router.put("/game/:idGame", editGame);
+
+
+router.put('/game/:idGame', async (req: Request, res: Response) => {
+  await editGame2(req.body, req.params.idGame)
+  return res.json()
+})
 
 // Route pour supprimer un jeu dans la BDD
-router.delete("/games/:idGame", async (req: Request, res: Response) => {
-  await deleteGame(req.params.idGame);
-  res.json();
-});
+router.delete('/games/:idGame', async (req: Request, res: Response) => {
+  await deleteGame(req.params.idGame)
+  res.json()
+})
 
 ////////////
 /// AUTH ///
@@ -45,28 +48,28 @@ router.delete("/games/:idGame", async (req: Request, res: Response) => {
 
 // Route qui gère l'authentification et différentes redirections
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/game/add",
-    failureRedirect: "/login/admin",
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/game/add',
+    failureRedirect: '/login/admin',
     failureFlash: true,
   })
-);
+)
 
-router.post("/login/user", usersController.login);
+router.post('/login/user', usersController.login)
 
 ////////////
 /// MEDIA ///
 ////////////
 
 // Route pour l'upload d'images
-router.post("/upload", upload.single("file"), uploadImages);
+router.post('/upload', upload.single('file'), uploadImages)
 
 ////////////
 /// USER ///
 ////////////
 
 // Route pour ajouter un user
-router.post("/users", usersController.addUser);
+router.post('/users', usersController.addUser)
 
-export default router;
+export default router
--- src/api/gamesController.ts
+++ src/api/gamesController.ts
@@ -1,23 +1,29 @@
-import { uploadToS3 } from "../../uploadFile";
-import game from "../models/gamesModel";
-import { Request, Response } from "express";
+import { uploadToS3 } from '../../uploadFile'
+import GameModel from '../models/gamesModel'
+import { Request, Response } from 'express'
+import { Game } from '../types'
 
-export const addGame = async (gameObject: any): Promise<Boolean> => {
-  const newGame = new game(gameObject);
+/**
+ * Ajoute un jeu dans la base de données
+ * @param gameObject
+ * @returns
+ */
+export const addGame = async (gameObject: Game): Promise<Boolean> => {
+  const newGame = new GameModel(gameObject)
 
   try {
-    const existingGame = await game.findOne({ nom: newGame.nom });
+    const existingGame = await GameModel.findOne({ nom: gameObject.nom })
     if (existingGame) {
-      return true;
+      return true
     } else {
-      await newGame.save();
-      return false;
+      await newGame.save()
+      return false
     }
   } catch (err) {
-    console.error(err);
-    throw err;
+    console.error(err)
+    throw err
   }
-};
+}
 
 /**
  * Supprime un jeu de la base de données
@@ -25,82 +31,92 @@ export const addGame = async (gameObject: any): Promise<Boolean> => {
  */
 export const deleteGame = async (gameId: string) => {
   try {
-    await game.findByIdAndRemove(gameId);
+    await GameModel.findByIdAndRemove(gameId)
   } catch (err) {
-    console.error("error suppression");
-    throw err;
-  }
-};
-
-export const editGame = async (req: Request, res: Response) => {
-  const idGame = req.params.idGame;
-
-  try {
-    const newGame = await game.findById(idGame);
-
-    if (!newGame) {
-      return res.redirect("/api/games");
-    }
-
-    newGame.nom = req.body.nom;
-    newGame.editeur = req.body.editeur;
-    // jeu.nbJoueurs = req.body.nbJoueurs;
-    // jeu.dureePartie = req.body.dureePartie;
-    // jeu.cooperatif = req.body.cooperatif;
-    newGame.categorie = req.body.categorie;
-
-    await newGame.save();
-    return res.status(200).send("");
-  } catch (err) {
-    console.error(err);
-    res.redirect("/api/games");
-  }
-};
-
-// Rechercher un jeu
-export const searchGames = async (req: Request, res: Response) => {
-  const searchQuery = req.query.query;
-
-  try {
-    const games = await game.find({
-      $or: [
-        { nom: { $regex: searchQuery, $options: "i" } }, // Recherche par nom (insensible à la casse)
-        { editeur: { $regex: searchQuery, $options: "i" } }, // Recherche par éditeur
-      ],
-    });
-
-    res.json(games);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Une erreur s'est produite lors de la recherche des jeux.",
-    });
-  }
-};
-
-export function uploadImages(req: Request, res: Response) {
-  console.log(req);
-  // Vérification si req.file existe et est défini
-  if (req.file) {
-    // Accéder au fichier téléchargé via req.file
-    const file = req.file;
-
-    // Utilisation de la fonction uploadToS3 pour envoyer le fichier vers S3
-    uploadToS3(file.path, req.file.originalname)
-      .then((result) => {
-        // Je récupère l'URL du fichier téléchargé depuis le résultat
-        const fileUrl = result.Location;
-        // Envoi un objet Json avec la propriété fileUrl
-        res.json({ fileUrl });
-      })
-      .catch((error) => {
-        // Gestion des erreurs lors de l'upload
-        res.status(500).send("Erreur lors de l'upload : " + error);
-      });
-  } else {
-    // Aucun fichier téléchargé, gestion de l'erreur
-    res.status(400).send("Aucun fichier téléchargé !");
+    console.error('error suppression')
+    throw err
   }
 }
 
-export default { deleteGame, editGame, searchGames };
+/**
+ * Modifie un jeu de la base de données
+ * @param gameObject un objet contenant un jeu (nom, categorie...)
+ * @param gameId l'id d'un jeu
+ */
+export const editGame2 = async (gameObject: {nom:string, categorie:string, editeur: string}, gameId:string) => {
+  await GameModel.findByIdAndUpdate(gameId, gameObject) 
+}
+
+// export const editGame = async (req: Request, res: Response) => {
+//   const idGame = req.params.idGame
+  
+
+//   try {
+//     const newGame = await GameModel.findById(idGame)
+
+//     if (!newGame) {
+//       return res.redirect('/api/games')
+//     }
+
+//     newGame.nom = req.body.nom
+//     newGame.editeur = req.body.editeur
+//     // jeu.nbJoueurs = req.body.nbJoueurs;
+//     // jeu.dureePartie = req.body.dureePartie;
+//     // jeu.cooperatif = req.body.cooperatif;
+//     newGame.categorie = req.body.categorie
+
+//     await newGame.save()
+//     return res.status(200).send('')
+//   } catch (err) {
+//     console.error(err)
+//     res.redirect('/api/games')
+//   }
+// }
+
+// Rechercher un jeu
+export const searchGames = async (req: Request, res: Response) => {
+  const searchQuery = req.query.query
+
+  try {
+    const games = await GameModel.find({
+      $or: [
+        { nom: { $regex: searchQuery, $options: 'i' } }, // Recherche par nom (insensible à la casse)
+        { editeur: { $regex: searchQuery, $options: 'i' } }, // Recherche par éditeur
+      ],
+    })
+
+    res.json(games)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error: "Une erreur s'est produite lors de la recherche des jeux.",
+    })
+  }
+}
+
+export function uploadImages(req: Request, res: Response) {
+  console.log(req)
+  // Vérification si req.file existe et est défini
+  if (req.file) {
+    // Accéder au fichier téléchargé via req.file
+    const file = req.file
+
+    // Utilisation de la fonction uploadToS3 pour envoyer le fichier vers S3
+    uploadToS3(file.path, req.file.originalname)
+      .then(result => {
+        // Je récupère l'URL du fichier téléchargé depuis le résultat
+        const fileUrl = result.Location
+        // Envoi un objet Json avec la propriété fileUrl
+        res.json({ fileUrl })
+      })
+      .catch(error => {
+        // Gestion des erreurs lors de l'upload
+        res.status(500).send("Erreur lors de l'upload : " + error)
+      })
+  } else {
+    // Aucun fichier téléchargé, gestion de l'erreur
+    res.status(400).send('Aucun fichier téléchargé !')
+  }
+}
+
+export default { deleteGame, searchGames, editGame2 }
