33b3e497baebd0f49f1f2fb0e33784b5a6e65792
PaulinedeValance <pauline.de.valance@gmail.com>
2023-10-16 10:24:00 +0200

    refactoring

--- src/pageRouter.ts
+++ src/pageRouter.ts
@@ -1,31 +1,27 @@
-import express from "express";
-import passport from "passport";
-import { categories } from "./models/gamesModel";
-import { translate } from "./services/translate";
-import displayGamesController from "./controllers/displayGamesController";
-import gamesController from "./api/gamesController";
-import requireAdmin from "./middleware/requireAdmin";
+import express from 'express'
+import { categories } from './models/gamesModel'
+import { translate } from './services/translate'
+import displayGamesController from './controllers/displayGamesController'
+import requireAdmin from './middleware/requireAdmin'
 
-const pageRouter = express.Router();
+const pageRouter = express.Router()
 
 // Route qui affiche le formulaire de connexion des admins
-pageRouter.get("/login/admin", (req: any, res: any) => {
-  const messages = req.flash("error");
-  res.render("connexionAdmin", { messages });
-}); 
+pageRouter.get('/login/admin', (req: any, res: any) => {
+  const messages = req.flash('error')
+  res.render('adminLogin', { messages })
+})
 
 // Route qui affiche un formulaire pour AJOUTER un jeu
-pageRouter.get("/game/add", requireAdmin, (req: any, res: any) => {
-  const tmp: any = [];
+pageRouter.get('/game/add', requireAdmin, (req: any, res: any) => {
+  const tmp: any = []
   categories.forEach((element: string) => {
-    tmp.push({ id: element, name: translate(element) });
-  });
-  res.render("addGamesForm", { categories: tmp });
-});
-
-pageRouter.get("/games/search", gamesController.searchGames);
+    tmp.push({ id: element, name: translate(element) })
+  })
+  res.render('addGamesForm', { categories: tmp })
+})
 
 // Route qui affiche la liste des jeux rentrés dans la BDD
-pageRouter.get("/games", requireAdmin, displayGamesController.displayGamesList);
+pageRouter.get('/games', requireAdmin, displayGamesController.displayGamesList)
 
-export default pageRouter;
+export default pageRouter
